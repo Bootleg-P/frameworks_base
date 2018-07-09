d03d4b00b3aa8d049f0bca1b5c8e0f7f469f7a1b
Jorim Jaggi <jjaggi@google.com>
2018-07-09 17:57:10 +0200

    Pin compiled code of HOME app
    Keeping the code in memory of the currently set home app is
    important for latency as we don't have any kind of starting
    window/splash screen when pressing the home app to hide any latency.
    
    Memory impact:
    
    Pinning dex/vdex:
    
    In practical scenarios, this should be < 500kb.
    The home app is usually profile-speed compiled, for which the
    resulting dex/vdex files are about 2 mb. However, during regular
    use, at least 1.5 MB of it is referenced in memory. This makes
    sense: By definition profile-speed only compiles the things that
    is usually frequently executed during regular execution.
    
    Pinning apk:
    With Launcher 3 in practical scenarios this should be about 3.7 MB,
    as the APK is about 5.7 MB but 2 MB are usually referenced in any
    case.
    
    Bug: 111132016
    Bug: 78585335
    Test: Inspect "adb shell dumpsys pinner" after boot.
    Test: Check for pinned files after updating camera/home.
    Test: Check for pinned files after user switch with different
    default apps.
    Test: Check for pinned files after bg-dexopt.
    Test: Check for pinned files after bg-dexopt + kill pid.
    
    Change-Id: I6cdbc06d089efeb1c72a51216879ba0573502009
    Merged-In: I6cdbc06d089efeb1c72a51216879ba0573502009

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -27022,6 +27022,13 @@ public class ActivityManagerService extends IActivityManager.Stub
         public void enforceCallerIsRecentsOrHasPermission(String permission, String func) {
             ActivityManagerService.this.enforceCallerIsRecentsOrHasPermission(permission, func);
         }
+
+        @Override
+        public Intent getHomeIntent() {
+            synchronized (ActivityManagerService.this) {
+                return ActivityManagerService.this.getHomeIntent();
+            }
+        }
     }
 
     /**
