648844f8a59efbb4b5c6dcc7b80d5cca5d3642fe
ezio84 <brabus84@gmail.com>
2018-12-11 20:44:13 +0100

    Reload assets on overlay change: code improvements
    don't do some of the refresh stuff that now on P is already triggered
    by onThemeChanged()
    this also fixes some colors after a theme change
    
    also remove useless mEntryManager.updateNotifications() that was calling mPresenter.updateNotificationViews()
    (so just StatusBar.updateNotificationViews()).
    
    
    Change-Id: I906192487761405a554a97e3d97cd36102bf5189

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
@@ -1006,6 +1006,10 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
         updateNotifications();
     }
 
+    public void onOverlayChanged() {
+        updateNotificationsOnDensityOrFontScaleChanged();
+    }
+
     protected boolean shouldPeek(NotificationData.Entry entry) {
         return shouldPeek(entry, entry.notification);
     }
