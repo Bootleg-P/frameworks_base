3522d1d3a11210b898a18b461f74f590f81cb82d
Alex <du.alexcruz@gmail.com>
2019-02-06 15:53:31 -0600

    Active edge settings [1/6]
    Change-Id: Idfbbf72955339b7deb87b55d917e496b307d11eb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9865,6 +9865,13 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Select various actions for squeeze gesture
+         *
+         * @hide
+         */
+        public static final String SQUEEZE_SELECTION = "squeeze_selection";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
