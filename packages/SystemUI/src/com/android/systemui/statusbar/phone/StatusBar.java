2aef0e2c61a3621f5df4e65b3b268f8c35ebf9fd
DennySPB <dennyspb@gmail.com>
2019-06-07 17:32:51 +0300

    GamingMode: Rewrite implementation [1/2]
    - move to GamingModeController
    - remove useless QS tile
    - depends only on running package names
    - track top-app activity change for enabling/disabling gaming mode
    - dynamic mode adds determined game into game list
    - make notification/toasts optional
    - update game list if package was uninstalled
    - remove volume maximize option (total mess if using bt headphones)
    - remove mess with dialer on AOD
    
    Change-Id: I4eeda8c96e068160400786e1f0f2451e69f8024c
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2241,10 +2241,6 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             return false;
         }
 
-        if (gamingModeOn && isGameAppDialer(sbn.getPackageName())) {
-            return true;
-        }
-
         if (mIsOccluded && !isDozing()) {
             boolean devicePublic = mLockscreenUserManager.
                     isLockscreenPublicMode(mLockscreenUserManager.getCurrentUserId());
@@ -2277,11 +2273,6 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             }
         }
         return true;
-    }
-
-    private boolean isGameAppDialer(String packageName) {
-        return packageName.equals("com.android.dialer")
-            || packageName.equals("com.google.android.dialer");
     }
 
     @Override  // NotificationData.Environment
