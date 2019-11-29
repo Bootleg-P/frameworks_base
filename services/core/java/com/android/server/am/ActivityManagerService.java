5c290d591dd2daae454ac9640be8312db2819ae9
varunhardgamer <gamelovr695@gmail.com>
2019-11-29 19:18:42 +0000

    Revert "Implement per-app thermal control [2/3]"
    This reverts commit 7b95c13535cd221fd14dd4c5b3d37a9a83a89f64.

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -27540,10 +27540,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         return null;
     }
 
-    Context getContext() {
-        return mContext;
-    }
-
     private class SwipeToScreenshotObserver extends ContentObserver {
 
         private final Context mContext;
