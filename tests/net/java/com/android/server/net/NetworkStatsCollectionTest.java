6c4e22bcad4d680a31cf463c2ec1bac3c8801ebf
Pavan Kumar M <rpavan@codeaurora.org>
2020-05-21 20:19:03 +0530

    Gracefully handle integer overflows.
    Avoid recording negative data by doing integer-based
    math as much as possible, but switch to double-based
    math if we detect that we'd end up causing an overflow.
    
    Test :
    
        - Builds, Boots
        - High data throughput scenarios
        - NetworkStatsHistoryTest, NetworkStatsCollectionTest
    
    Bug: 119527458
    Change-Id: I55d4aa649bea3ae201ad513074ff2b71cb918dad
    CRs-Fixed: 2608836
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- tests/net/java/com/android/server/net/NetworkStatsCollectionTest.java
+++ tests/net/java/com/android/server/net/NetworkStatsCollectionTest.java
@@ -501,26 +501,6 @@ public class NetworkStatsCollectionTest {
         assertEquals(TIME_A - HOUR_IN_MILLIS, coll.roundDown(TIME_A - 1));
     }
 
-    @Test
-    public void testMultiplySafe() {
-        assertEquals(25, multiplySafe(50, 1, 2));
-        assertEquals(100, multiplySafe(50, 2, 1));
-
-        assertEquals(-10, multiplySafe(30, -1, 3));
-        assertEquals(0, multiplySafe(30, 0, 3));
-        assertEquals(10, multiplySafe(30, 1, 3));
-        assertEquals(20, multiplySafe(30, 2, 3));
-        assertEquals(30, multiplySafe(30, 3, 3));
-        assertEquals(40, multiplySafe(30, 4, 3));
-
-        assertEquals(100_000_000_000L,
-                multiplySafe(300_000_000_000L, 10_000_000_000L, 30_000_000_000L));
-        assertEquals(100_000_000_010L,
-                multiplySafe(300_000_000_000L, 10_000_000_001L, 30_000_000_000L));
-        assertEquals(823_202_048L,
-                multiplySafe(4_939_212_288L, 2_121_815_528L, 12_730_893_165L));
-    }
-
     /**
      * Copy a {@link Resources#openRawResource(int)} into {@link File} for
      * testing purposes.
